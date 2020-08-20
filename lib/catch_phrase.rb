47805533d212548bfc087754ab521733d36676c3
William T Cao <williamacao@gmail.com>
2020-08-20 20:18:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,4 +1,11 @@
+catch_phrase = 
 def catch_phrase
   puts phrase
 end
 
+evil_monster = "Bowser"
+ 
+def princess_peaches_castle(evil_monster)
+  puts "#{evil_monster} is trying to kidnap Princess Peach!"
+end
+ 
